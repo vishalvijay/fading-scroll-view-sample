cd0ec61f56e467d57d2b203bf3ea9acd416a619d
Vishal Vijay <0vishalvijay0@gmail.com>
2014-10-01 00:37:09 +0530

    initial commit

---
+++ src/com/example/faidingscrollview/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.faidingscrollview;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class MainActivity extends ListActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		String[] names = { "First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second", "First message second",
+				"First message second" };
+		setListAdapter(new ArrayAdapter<String>(this, R.layout.text_list, names));
+	}
+}
